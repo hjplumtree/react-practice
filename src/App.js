7073d1dd431290eb5a75a89278f73c94cf53ee5d
hej <dark199174@gmail.com>
2021-12-28 11:48:00 +0900

    기본 리액트 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-import logo from "./logo.svg";
 import "./App.css";
 
+function HeaderTag() {
+  return (
+    <header>
+      <h1>
+        <a href="index.html">React</a>
+      </h1>
+    </header>
+  );
+}
+
+function NavTag() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="1.html">html</a>
+        </li>
+        <li>
+          <a href="2.html">css</a>
+        </li>
+        <li>
+          <a href="3.html">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function ArticleTag() {
+  return (
+    <article>
+      <h2>Welcome!</h2>
+      Hello, WEB!
+    </article>
+  );
+}
 function App() {
-  return <div className="App">안녕, 리액트!</div>;
+  return (
+    <div className="App">
+      <HeaderTag></HeaderTag>
+      <NavTag></NavTag>
+      <ArticleTag></ArticleTag>
+    </div>
+  );
 }
 
 export default App;
