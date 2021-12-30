c64f59d8f145f60c6e9deff9e628fefcb9817f96
hej <dark199174@gmail.com>
2021-12-30 11:02:35 +0900

    Update to egoing code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import "./App.css";
 
-function App() {
-  let topics = [
-    { id: 1, title: "html", body: "html is ..." },
-    { id: 2, title: "css", body: "css is ..." },
-    { id: 3, title: "js", body: "js is ..." },
-  ];
-
-  function onChangeModeHandler(mode) {
-    alert(mode);
-  }
-
-  return (
-    <div className="App">
-      <Header title="Web" onChangeMode={onChangeModeHandler} />
-      <Nav data={topics} onChangeMode={onChangeModeHandler} />
-      <Article title="Welcome" body="Hello, React!" />
-    </div>
-  );
-}
-
 function Header(props) {
+  console.log(props.onChangeMode);
   function onClickHandler(evt) {
     evt.preventDefault();
-    props.onChangeMode(1);
+    props.onChangeMode();
   }
   return (
     <header>
@@ -35,23 +16,22 @@ function Header(props) {
     </header>
   );
 }
-
 function Nav(props) {
-  function onClickHandler(evt) {
+  let lis = [];
+  function clickHandler(evt) {
     evt.preventDefault();
-    props.onChangeMode("READ");
+    props.onChangeMode();
   }
-
-  const lis = props.data.map((d) => {
-    return (
+  for (let i = 0; i < props.data.length; i = i + 1) {
+    let d = props.data[i];
+    lis.push(
       <li key={d.id}>
-        <a href={"/read/" + d.id} onClick={onClickHandler}>
+        <a href={"/read/" + d.id} onClick={clickHandler}>
           {d.title}
         </a>
-      </li>
+      </li>,
     );
-  });
-
+  }
   return (
     <nav>
       <ol>{lis}</ol>
@@ -64,6 +44,26 @@ function Article(props) {
       <h2>{props.title}</h2>
       {props.body}
     </article>
+  );
+}
+function App() {
+  let topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "js", body: "js is ..." },
+  ];
+  function onChangeModeHandler() {
+    alert("change!");
+  }
+  function onChangeNavModeHandler() {
+    alert("changeNav!");
+  }
+  return (
+    <>
+      <Header title="WEB" onChangeMode={onChangeModeHandler} />
+      <Nav data={topics} onChangeMode={onChangeNavModeHandler} />
+      <Article title="Welcome" body="Hello, React!" />
+    </>
   );
 }
 
